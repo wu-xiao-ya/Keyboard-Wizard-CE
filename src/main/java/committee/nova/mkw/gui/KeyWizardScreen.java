907bc3fbecef193c5e75763f097ea0e0f772c9d4
兀小雅 <zpyn1818@outlook.com>
2026-07-11 23:36:18 +0800

    Fix Fabric 1.21.1 screen API boundary

--- src/main/java/committee/nova/mkw/gui/KeyWizardScreen.java
+++ src/main/java/committee/nova/mkw/gui/KeyWizardScreen.java
@@ -217,6 +217,10 @@ public class KeyWizardScreen extends GameOptionsScreen {
     }
 
     @Override
+    protected void addOptions() {
+    }
+
+    @Override
     public void tick() {
         for (Element child : this.children()) {
             if (child instanceof TickableElement tickableElement) {
